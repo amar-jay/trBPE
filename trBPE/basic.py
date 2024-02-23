d3bfa01c27a1474468acd0df5c7c2833afa09066
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-02-23 22:55:18 +0300

    Adding save, load fuctionalities

---
+++ trBPE/basic.py
@@ -0,0 +1,141 @@
+# Not an exact copy of the original file, but a simplified version of the original file
+# https://github.com/openai/tiktoken/blob/main/tiktoken/_educational.py
+from typing import Optional
+import collections
+
+import regex
+
+
+# this is the max size of a byte,
+# since vocab = all sing bytes + byte merges 
+MIN_VOCAB_SIZE=256 
+
+
+class BasicBPE:
+    """
+    Basic Implementation of Byte Pair Encoding (BPE) 
+    In this implementation we are not doing any merging
+    """
+
+    def __init__(self, vocab_size, pattern = "", special_tokens = None):
+        self.pattern = regex.compile(pattern)
+        self.special_tokens = special_tokens or {}
+        self.ranks = self._build_ranks() 
+        self.decoder = {v: k for k, v in self.encoder.items()}
+
+    def _build_ranks(self):
+        """
+        It builds the key value encoding of all bytes,
+        an byte to integer encoding
+            """
+        atoi = {bytes([idx]): idx for idx in range(MIN_VOCAB_SIZE)}
+        for token, idx in self.special_tokens:
+            atoi[token] = idx
+        return atoi 
+
+    def train(self, text:str, vocab_size: int, pattern: str) -> list[list[bytes]]:
+        if vocab_size < MIN_VOCAB_SIZE:
+            raise ValueError("Vocab size must be > 2^8")
+
+
+        ranks = self.ranks
+        # Splinter up our data into lists of bytes
+        # data = "Hello world"
+        # words = [
+        #     [b'H', b'e', b'l', b'l', b'o'],
+        #     [b' ', b'w', b'o', b'r', b'l', b'd']
+        # ]
+        words = regex.findall(self.pattern, text)
+        vocabs: list[list[bytes]] = [
+            [bytes([b]) for b in str(word).encode("utf-8")] for word in words
+        ]
+
+
+        # using our data, we will figure out what we should merges based on count
+        n = len(ranks)
+        while n < vocab_size:
+            counter = collections.Counter()
+            for word in vocabs:
+                for pair in zip(word[-1], word[1:]):
+                    counter[pair] += 1
+
+            most_common_pair = max(counter, key=lambda i: counter[i])
+
+            token = bytes(most_common_pair[0] + most_common_pair[1])
+            ranks[token] = n # adding token at n
+
+            # update our vocabs to include tokens
+            new_words = []
+            for word in words:
+                new_word = []
+                i = 0
+                while i < len(vocabs) - 1:
+                    if (word[i], word[i+1]) == most_common_pair:
+                        new_word.append(token)
+                        i+=2
+                    else:
+                        new_word.append(word[i])
+                        i+=1
+                if i == len(word) - 1:
+                    i+=1
+                new_words.append(new_word)
+            vocabs = new_words
+
+        self.ranks = ranks
+
+        return vocabs
+
+    def _save_model(self, vocabs:list[list[bytes]], file: str = "model"):
+        """
+        Similar to sentencepiece in llama, we save it in *.model. 
+        https://github.com/google/sentencepiece/blob/a216bd01d166ca2b26ad5167b54dd8d51f416ae9/src/trainer_interface.cc#L698
+        """
+        print("Saving vocabs to ", file + ".model")
+        pass
+
+    def _save_vocab(self, vocabs:list[list[bytes]], file: str = "model"):
+        """
+        Similar to sentencepiece in llama, we save it in *.vocab. 
+        https://github.com/google/sentencepiece/blob/a216bd01d166ca2b26ad5167b54dd8d51f416ae9/src/trainer_interface.cc#L698
+        """
+
+        print("Saving vocabs to ", file + ".vocab")
+        with open(file + ".vocab", "w+") as f:
+            for i in enumerate(vocabs):
+                f.write()
+
+
+
+    def save(self, vocabs:list[list[bytes]], file: str = "model"):
+        try:
+            self._save_model(vocabs, file)
+            self._save_vocab(vocabs, file)
+        except Exception as e:
+            raise e
+
+        
+        for word in words:
+            word_bytes = word.encode("utf-8")
+            tokens.append(self._encode_word(word_bytes))
+            word_tokens = self.encoder.get(word_bytes)
+        
+        return tokens
+    def train(self):
+        pass
+
+    def _encode_word(self, word: str) -> dict[bytes, int]:
+        # check size of word
+        ranks = self.encoder
+
+
+if __name__ == "__main__":
+    vocab_size = 600
+    gpt4_pattern = r""
+    
+    with open("sample.txt", "r") as f:
+        data = f.read()
+
+    enc = BasicBPE.train(data, vocab_size, gpt4_pattern)
+    print(enc)
+
+    pass
